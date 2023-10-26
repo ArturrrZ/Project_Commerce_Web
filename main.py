d302368243d60438a73aa85fa6fd7f9c8caa0291
Artur Z <arturziianbaev@gmail.com>
2023-10-26 15:43:54 -0700

    create header in father template and connect with home page

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from flask import Flask, request,render_template,redirect
 
+from flask_bootstrap import Bootstrap5
 app=Flask(__name__)
 
-
+Bootstrap5(app)
 
 @app.route("/")
 def home():
     return render_template('home.html')
+
+@app.route("/check_father")
+def check():
+    return render_template("father_template.html")
+
+
 
 
 
