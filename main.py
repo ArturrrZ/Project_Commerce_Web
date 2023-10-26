45a0a0c95e1336d204c24cf9fee7a7a03874551e
Artur Z <arturziianbaev@gmail.com>
2023-10-26 15:06:51 -0700

    connect home page

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask, request,render_template,redirect
+
+app=Flask(__name__)
+
+
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
