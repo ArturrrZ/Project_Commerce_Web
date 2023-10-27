a1ff7bb1f05251372407d8d462e6e7389c0667e1
Artur Z <arturziianbaev@gmail.com>
2023-10-27 10:27:22 -0700

    connect second page and anchors on the first and show carousel of pictures of an item

--- main.py
+++ main.py
@@ -5,6 +5,67 @@ app=Flask(__name__)
 
 Bootstrap5(app)
 
+ITEMS_FOR_SALE={
+    "chair": {
+        "title": "Black Computer Chair",
+        "price": "30$",
+        "pic_1": "./static/assets/img/items_for_sale/chair_1.jpg",
+        "pic_2": "./static/assets/img/items_for_sale/chair_2.jpg",
+        "pic_3": "./static/assets/img/items_for_sale/chair_3.jpg",
+    },
+    "table": {
+        "title": "Glass Table",
+        "price": "50$",
+        "pic_1": "./static/assets/img/items_for_sale/table_1.jpg",
+        "pic_2": "./static/assets/img/items_for_sale/table_2.jpg",
+        "pic_3": "./static/assets/img/items_for_sale/table_3.jpg"
+
+    },
+    "laptop": {
+        "title": "ASUS TUF Dash F15 Gaming Laptop",
+        "price": "1000$",
+        "pic_1": "./static/assets/img/items_for_sale/laptop_1.jpg",
+        "pic_2": "./static/assets/img/items_for_sale/laptop_2.jpg",
+        "pic_3": "./static/assets/img/items_for_sale/laptop_3.jpg"
+
+    },
+    "mouse": {
+        "title": "Logitech G305 Wireless Gaming Mouse",
+        "price": "20$",
+        "pic_1": "./static/assets/img/items_for_sale/mouse_1.jpg",
+        "pic_2 ": "./static/assets/img/items_for_sale/mouse_2.jpg",
+        "pic_3": "./static/assets/img/items_for_sale/mouse_3.jpg",
+
+    },
+    "mousepad":
+        {
+        "title": "Mouse Pad",
+        "price": "10$",
+        "pic_1": "./static/assets/img/items_for_sale/mousepad_1.jpg",
+        "pic_2": "./static/assets/img/items_for_sale/mousepad_2.jpg",
+        "pic_3": "./static/assets/img/items_for_sale/mousepad_3.jpg",
+        },
+    "book":
+        {
+        "title": "Atomic Habits by J.Clear",
+        "price": "10$",
+        "pic_1": "./static/assets/img/items_for_sale/book_1.jpg",
+        "pic_2": "./static/assets/img/items_for_sale/book_2.jpg",
+        "pic_3": "./static/assets/img/items_for_sale/book_3.jpg"
+        },
+    "notebook":
+        {
+        "title": "Study Notebook",
+        "price": "99$",
+        "pic_1": "./static/assets/img/items_for_sale/notebook_1.jpg",
+        "pic_2": "./static/assets/img/items_for_sale/notebook_2.jpg",
+        "pic_3": "./static/assets/img/items_for_sale/notebook_3.jpg"
+        }
+
+}
+
+
+
 @app.route("/")
 def home():
     return render_template('home.html')
@@ -15,7 +76,14 @@ def check():
 
 @app.route("/items")
 def display_items():
-    return render_template("items.html")
+    return render_template("items.html", items=ITEMS_FOR_SALE)
+
+
+@app.route("/sale/<item>")
+def sale(item):
+    print(ITEMS_FOR_SALE[item])
+    return render_template('item_separate.html', item=ITEMS_FOR_SALE[item],name=item)
+
 
 
 
